bf777213a325048af0db882c35a222a73bb7fb91
Fredrick Brennan <copypaste@kittens.ph>
2022-01-10 22:11:51 -0500

    Move tests to own directory

--- src/lib.rs
+++ src/lib.rs
@@ -270,31 +270,3 @@ impl TryFrom<&str> for IntegerOrFloat {
         }
     }
 }
-
-mod tests {
-    #[test]
-    fn test_impls() {
-        use crate::IntegerOrFloat::{self, *};
-
-        assert_eq!(Integer(3) * Float(-1.0), Float(-3.0));
-        assert_eq!(Integer(3) * Integer(3), Integer(9));
-        assert_eq!(Integer(5) % Integer(2), Integer(1));
-        assert_eq!(Float(9.0) - Float(7.0), Float(2.0));
-        assert_eq!(-Float(4.0), Float(-4.0));
-        assert_eq!(Integer(5) * 5.0, Float(25.0));
-        assert_eq!(Float(5.0) * 5, Float(25.0));
-        assert_eq!(Integer(5) * 5, Integer(25));
-        let x: usize = Integer(5).into();
-        assert_eq!(x, 5);
-        let x: isize = Integer(-5).into();
-        assert_eq!(x, -5);
-        let x: f64 = Integer(5).into();
-        assert_eq!(x, 5.0);
-
-        let x: IntegerOrFloat = (4.5).into();
-        assert_eq!(x, Float(4.5));
-
-        let x: IntegerOrFloat = (10).into();
-        assert_eq!(x, Integer(10));
-    }
-}
